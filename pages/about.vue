8f38b326334f7ea26b02932087f1850b4375b413
chaderenyore <chaderenyore480@gmail.com>
2022-05-17 12:55:14 +0100

    Add about page texts

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,10 @@
 <template>
 	 <div class="about-page">
-	 	<h1>About Page</h1>	
+	 	<h1 class="text-xl font-bold">About NuxtBlog</h1>
+	 	<p>This Blog was built by <a href="https://github.com/chaderenyore" target="_blank" class="text-[#9f25cf]">chaderenyore</a> Using the Nuxt Content!</p>	
+	 	<p>it is very simple and straightforward, you can contribute to this repo by making a pull 
+	 	request. 
+	 	</p>
 	 </div>	
 
 </template>
